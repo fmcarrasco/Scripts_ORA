fd177035dc1cb526ab82ffc7cc5784becaf5249f
Félix Carrasco <felix.marcial@gmail.com>
2025-12-19 11:39:38 -0300

    Actualizacion lib PP_T

---
+++ Seguimiento_PP_T/lib/oramdb_opfunc.py
@@ -0,0 +1,36 @@
+import pyodbc
+import pandas as pd
+import numpy as np
+# General parameters to read database
+
+def pp_sql_string(id, idtipo, fecha_i, fecha_f, idemp=None):
+    """
+    This function generate the strings to get data from the
+    database of ORA (ora.mdb)
+    """
+    SQL_pp = '''
+            SELECT Fecha, Precipitacion, IdEstadoPrecip FROM DatoDiario
+            WHERE Estacion = {}
+            AND (((DatoDiario.Fecha)>=#{}#))
+            AND (((DatoDiario.Fecha)<=#{}#))
+            ORDER BY Fecha
+            '''.format(id, fecha_i, fecha_f)
+    SQL_ipp = '''
+            SELECT Fecha, Valor FROM DatoInterpolado
+            WHERE Estacion = {}
+            AND (((DatoInterpolado.Fecha)>=#{}#))
+            AND (((DatoInterpolado.Fecha)<=#{}#))
+            AND (((DatoInterpolado.nombreCampo)=1))
+            ORDER BY Fecha
+            '''.format(id, fecha_i, fecha_f)
+    if idemp is not None:
+        SQL_epp = '''
+                SELECT Fecha, Precipitacion, IdEstadoPrecip FROM DatoDiario
+                WHERE Estacion = {}
+                AND (((DatoDiario.Fecha)>=#{}#))
+                AND (((DatoDiario.Fecha)<=#{}#))
+                ORDER BY Fecha
+                '''.format(idemp, fecha_i, fecha_f)
+        return SQL_pp, SQL_ipp, SQL_epp
+    else:
+        return SQL_pp, SQL_ipp
